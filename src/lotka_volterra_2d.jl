5742f7ae523520614be7a0b693bd8ab15a5f2d17
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-02-07 16:41:21 +0100

    Add plot recipes for Lotka-Volterra 2D problem.

--- src/lotka_volterra_2d.jl
+++ src/lotka_volterra_2d.jl
@@ -1,5 +1,7 @@
 module LotkaVolterra2d
 
+    using Plots: @layout
+    using RecipesBase
     using Reexport
     using GeometricIntegrators.Solutions
 
@@ -9,6 +11,7 @@ module LotkaVolterra2d
 
     export hamiltonian, ϑ, ϑ₁, ϑ₂, ω
     export compute_energy_error, compute_momentum_error
+    export f
 
 
     function compute_energy_error(t, q::DataSeries{T}) where {T}
@@ -33,6 +36,75 @@ module LotkaVolterra2d
         end
 
         (p1_error, p2_error)
+    end
+
+
+    @userplot PlotLotkaVolterra2d
+    @recipe function f(p::PlotLotkaVolterra2d)
+        if length(p.args) != 1 || !(typeof(p.args[1]) <: Solution)
+            error("Lotka-Volterra plots should be given a solution. Got: $(typeof(p.args))")
+        end
+        sol = p.args[1]
+
+        legend := :none
+        size := (800,400)
+
+        H, ΔH = compute_energy_error(sol.t, sol.q);
+
+        # solution
+        layout := @layout [solPlot{0.4w,1.0h} EPlot]
+
+        @series begin
+            xlabel := "x₁"
+            ylabel := "x₂"
+            aspectratio := 1
+            subplot := 1
+            sol.q[1,:], sol.q[2,:]
+        end
+
+        @series begin
+            xlabel := "t"
+            ylabel := "[H(t) - H(0)] / H(0)"
+            subplot := 2
+            sol.t, ΔH
+        end
+    end
+
+
+    @userplot PlotLotkaVolterra2dTraces
+    @recipe function f(p::PlotLotkaVolterra2dTraces)
+        if length(p.args) != 1 || !(typeof(p.args[1]) <: Solution)
+            error("Lotka-Volterra plots should be given a solution. Got: $(typeof(p.args))")
+        end
+        sol = p.args[1]
+
+        legend := :none
+        size := (800,600)
+
+        H, ΔH = compute_energy_error(sol.t, sol.q);
+
+        # traces
+        layout := @layout [x₁Plot
+                           x₂Plot
+                           EPlot]
+
+        ylabels = ("x₁", "x₂")
+
+        for i in 1:2
+            @series begin
+                # xlabel := "t"
+                ylabel := ylabels[i]
+                subplot := i
+                sol.t, sol.q[i,:]
+            end
+        end
+
+        @series begin
+            xlabel := "t"
+            ylabel := "[H(t) - H(0)] / H(0)"
+            subplot := 3
+            sol.t, ΔH
+        end
     end
 
 end
