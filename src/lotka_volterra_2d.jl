3531dfbe61a9170bdfbcf9efa200cc0817688170
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-02-10 10:11:16 +0100

    Move energy and momentum diagnostics to diagnostics module.

--- src/lotka_volterra_2d.jl
+++ src/lotka_volterra_2d.jl
@@ -3,39 +3,25 @@ module LotkaVolterra2d
     using Plots: @layout
     using RecipesBase
     using Reexport
-    using GeometricIntegrators.Solutions
 
+    @reexport using ..Diagnostics
     @reexport using GeometricIntegrators.TestProblems.LotkaVolterra2dProblem
 
-    using GeometricIntegrators.TestProblems.LotkaVolterra2dProblem: hamiltonian, ϑ, ϑ₁, ϑ₂, ω
+    import GeometricIntegrators.TestProblems.LotkaVolterra2dProblem: hamiltonian, ϑ, ϑ₁, ϑ₂, ω
 
     export hamiltonian, ϑ, ϑ₁, ϑ₂, ω
     export compute_energy_error, compute_momentum_error
     export f
 
 
-    function compute_energy_error(t, q::DataSeries{T}) where {T}
-        h = SDataSeries(T, q.nt)
-        e = SDataSeries(T, q.nt)
-
-        for i in axes(q,2)
-            h[i] = hamiltonian(t[i], q[:,i])
-            e[i] = (h[i] - h[0]) / h[0]
+    function ϑ(t::Number, q::Vector, k::Int)
+        if k == 1
+            ϑ₁(t, q)
+        elseif k == 2
+            ϑ₂(t, q)
+        else
+            nothing
         end
-
-        (h, e)
-    end
-
-    function compute_momentum_error(t, q, p)
-        p1_error = zeros(q.nt+1)
-        p2_error = zeros(q.nt+1)
-
-        for i in 1:(q.nt+1)
-            p1_error[i] = p.d[1,i] - ϑ₁(t.t[i], q.d[:,i])
-            p2_error[i] = p.d[2,i] - ϑ₂(t.t[i], q.d[:,i])
-        end
-
-        (p1_error, p2_error)
     end
 
 
