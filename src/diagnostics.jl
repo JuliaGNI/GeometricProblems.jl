c05024d2b4c886ae70cce4f3ce443ea5dadb81b0
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-02-11 22:03:56 +0100

    Add general diagnostics.

--- src/diagnostics.jl
+++ src/diagnostics.jl
@@ -1,9 +1,18 @@
 module Diagnostics
 
+    using Plots
+    using Plots.PlotMeasures
+    # using Plots: convert_sci_unicode
+    using Showoff
+    using Showoff: format_fixed_scientific
     using RecipesBase
+
     using GeometricIntegrators.Solutions
 
     export compute_energy_error, compute_momentum_error, compute_error_drift
+
+
+    subscript(i::Integer) = i<0 ? error("$i is negative") : join('₀'+d for d in reverse(digits(i)))
 
 
     function compute_one_form(t::TimeSeries, q::DataSeries, one_form::Function=ϑ)
@@ -42,48 +51,165 @@ module Diagnostics
         return e
     end
 
-    function compute_error_drift(e::DataSeries{T,1}, lint=100) where {T}
-        nint = div(e.nt, lint)
+    function compute_error_drift(t::TimeSeries, e::DataSeries{T,1}, lint=100) where {T}
+        @assert mod(t.n, lint) == 0
 
-        # d = SDataSeries(T, nint)
-        #
-        # for i in 1:nint
-        #     i1 = lint*(i-1)+1
-        #     i2 = lint*i
-        #     d[i] = maximum(e[i1:i2])
-        # end
-        #
-        # d .-= d[0]
+        nint   = div(e.nt, lint)
+        Tdrift = TimeSeries(nint, t.Δt*lint)
+        Edrift = SDataSeries(T, nint)
 
-        drift = zeros(T, nint)
+        compute_timeseries!(Tdrift, t[0])
 
         for i in 1:nint
             i1 = lint*(i-1)+1
             i2 = lint*i
-            drift[i] = maximum(e[i1:i2])
+            Edrift[i] = maximum(e[i1:i2])
         end
 
-        drift .- drift[1]
+        (Tdrift, Edrift)
+    end
+
+
+    function energy_ticks(ΔH)
+        ticks = optimize_ticks(extrema(ΔH)...; k_min=4)[1]
+        # labels = Showoff.showoff(ticks, :scientific)
+        labels = format_fixed_scientific.(ticks, 2, false)
+
+        # if backend_name() == :gr
+        #     # for GR
+        #     yticks = (ticks, convert_sci_unicode.(labels))
+        # else
+        #     # for most backends
+        #     yticks = (ticks, labels)
+        # end
+        # yticks
+
+        (ticks, labels)
     end
 
 
     @userplot PlotEnergyError
     @recipe function f(p::PlotEnergyError)
-        if length(p.args) != 1 || !(typeof(p.args[1]) <: Solution)
-            error("Energy error plot should be given a solution. Got: $(typeof(p.args))")
+        if length(p.args) == 1 && typeof(p.args[1]) <: Solution
+            sol = p.args[1]
+            t = sol.t
+            H, ΔH = compute_energy_error(sol.t, sol.q)
+        elseif length(p.args) == 2 && typeof(p.args[1]) <: TimeSeries && typeof(p.args[2]) <: DataSeries
+            t  = p.args[1]
+            ΔH = p.args[2]
+            @assert length(t) == length(ΔH)
+        else
+            error("Energy error plot should be given a solution or a timeseries and a data series. Got: $(typeof(p.args))")
         end
-
-        sol = p.args[1]
 
         legend := :none
         size := (800,400)
 
-        H, ΔH = compute_energy_error(sol.t, sol.q)
-
         @series begin
             xlabel := "t"
             ylabel := "[H(t) - H(0)] / H(0)"
-            sol.t, ΔH
+            yticks := energy_ticks(ΔH)
+            right_margin := 10mm
+            xlims  := (t[0], Inf)
+            yformatter := :scientific
+            # widen := false
+            t, ΔH
+        end
+    end
+
+
+    @userplot PlotEnergyDrift
+    @recipe function f(p::PlotEnergyDrift)
+        if length(p.args) == 2 && typeof(p.args[1]) <: TimeSeries && typeof(p.args[2]) <: DataSeries
+            t = p.args[1]
+            d = p.args[2]
+            @assert length(t) == length(d)
+        else
+            error("Energy drift plot should be given a timeseries and a data series. Got: $(typeof(p.args))")
+        end
+
+        legend := :none
+        size := (800,400)
+
+        @series begin
+            xlabel := "t"
+            ylabel := "ΔH Drift"
+            # yticks := energy_ticks(ΔH)
+            right_margin := 10mm
+            xlims  := (t[0], Inf)
+            yformatter := :scientific
+            # widen := false
+            t, d
+        end
+    end
+
+
+    @userplot PlotMomentumError
+    @recipe function f(p::PlotMomentumError)
+        if length(p.args) == 1 && typeof(p.args[1]) <: Solution
+            sol = p.args[1]
+            t = sol.t
+            Δp = compute_momentum_error(sol.t, sol.q, sol.p)
+        elseif length(p.args) == 2 && typeof(p.args[1]) <: TimeSeries && typeof(p.args[2]) <: DataSeries
+            t  = p.args[1]
+            Δp = p.args[2]
+            @assert t.n == Δp.nt
+        else
+            error("Momentum error plots should be given a solution or a timeseries and a data series. Got: $(typeof(p.args))")
+        end
+
+        legend := :none
+        size := (800,200*Δp.nd)
+
+        # traces
+        layout := (Δp.nd,1)
+
+        for i in 1:Δp.nd
+            @series begin
+                subplot := i
+                if i == Δp.nd
+                    xlabel := "t"
+                end
+                ylabel := "p" * subscript(i) * "(t) - ϑ" * subscript(i) * "(t)"
+                xlims  := (t[0], Inf)
+                right_margin := 10mm
+                t, Δp[i,:]
+            end
+        end
+    end
+
+
+    @userplot PlotLagrangeMultiplier
+    @recipe function f(p::PlotLagrangeMultiplier)
+        if length(p.args) == 1 && typeof(p.args[1]) <: Solution
+            sol = p.args[1]
+            t = sol.t
+            λ = sol.λ
+        elseif length(p.args) == 2 && typeof(p.args[1]) <: TimeSeries && typeof(p.args[2]) <: DataSeries
+            t = p.args[1]
+            λ = p.args[2]
+            @assert t.n == λ.nt
+        else
+            error("Lagrange multiplier plots should be given a solution or a timeseries and a data series. Got: $(typeof(p.args))")
+        end
+
+        legend := :none
+        size := (800,200*λ.nd)
+
+        # traces
+        layout := (λ.nd,1)
+
+        for i in 1:λ.nd
+            @series begin
+                subplot := i
+                if i == λ.nd
+                    xlabel := "t"
+                end
+                ylabel := "λ" * subscript(i) * "(t)"
+                xlims  := (t[0], Inf)
+                right_margin := 10mm
+                t, λ[i,:]
+            end
         end
     end
 
