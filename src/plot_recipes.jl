0f60d99c73de86dbacdeb6b2d11e268cf33bb666
Michael Kraus <michael.kraus@ipp.mpg.de>
2020-09-22 13:57:44 +0200

    Fix plot recipes.

--- src/plot_recipes.jl
+++ src/plot_recipes.jl
@@ -5,7 +5,7 @@ module PlotRecipes
     using RecipesBase
     using LaTeXStrings
 
-    using GeometricIntegrators.Solutions: DataSeries, TimeSeries, Solution
+    using GeometricIntegrators.Solutions
     using ..Diagnostics
 
 
@@ -17,7 +17,11 @@ module PlotRecipes
         if length(p.args) == 1 && typeof(p.args[1]) <: Solution
             sol = p.args[1]
             t = sol.t
-            H, ΔH = compute_invariant_error(sol.t, sol.q, energy)
+            if typeof(sol) <: Union{SolutionPODE, SolutionPDAE}
+                H, ΔH = compute_invariant_error(sol.t, sol.q, sol.p, energy)
+            else
+                H, ΔH = compute_invariant_error(sol.t, sol.q, energy)
+            end
         elseif length(p.args) == 2 && typeof(p.args[1]) <: TimeSeries && typeof(p.args[2]) <: DataSeries
             t  = p.args[1]
             ΔH = p.args[2]
@@ -132,7 +136,7 @@ module PlotRecipes
 
 
     @userplot PlotLagrangeMultiplier
-    @recipe function f(p::PlotLagrangeMultiplier; nplot=1, latex=true)
+    @recipe function f(p::PlotLagrangeMultiplier; k=0, nplot=1, latex=true)
         if length(p.args) == 1 && typeof(p.args[1]) <: Solution
             sol = p.args[1]
             t = sol.t
@@ -145,33 +149,57 @@ module PlotRecipes
             error("Lagrange multiplier plots should be given a solution or a timeseries and a data series. Got: $(typeof(p.args))")
         end
 
+        ntrace = (k == 0 ?    λ.nd  :    1 )
+        trange = (k == 0 ? (1:λ.nd) : (k:k))
+
+        size   := (800,200*ntrace)
         legend := :none
-        size := (800,200*λ.nd)
+        layout := (ntrace,1)
 
-        # traces
-        layout := (λ.nd,1)
+        right_margin  := 10mm
 
-        for i in 1:λ.nd
+        if ntrace == 1 && backend() == Plots.GRBackend()
+            left_margin := 10mm
+        end
+
+        guidefont := font(12)
+        tickfont  := font(10)
+
+        for i in trange
             @series begin
-                subplot := i
-                if i == λ.nd
+                if k == 0
+                    subplot := i
+                end
+                if i == λ.nd || k ≠ 0
                     if latex
                         xguide := L"t"
                     else
                         xguide := "t"
                     end
+                    bottom_margin := 10mm
                 else
                     xaxis := false
                 end
+                # if i < λ.nd && k == 0
+                #     if backend() == Plots.PGFPlotsXBackend()
+                #         bottom_margin := -6mm
+                #     elseif backend() == Plots.GRBackend()
+                #         bottom_margin := -3mm
+                #     end
+                # end
+                # if i > 1 && k == 0
+                #     if backend() == Plots.PGFPlotsXBackend()
+                #         top_margin := -6mm
+                #     elseif backend() == Plots.GRBackend()
+                #         top_margin := -3mm
+                #     end
+                # end
                 if latex
                     yguide := latexstring("\\lambda_$i (t)")
                 else
                     yguide := "λ" * subscript(i) * "(t)"
                 end
                 xlims  := (t[0], Inf)
-                guidefont := font(18)
-                tickfont := font(12)
-                right_margin := 10mm
                 t[0:nplot:end], λ[i,0:nplot:end]
             end
         end
