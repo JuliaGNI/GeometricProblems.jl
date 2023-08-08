7c8ac49bfe6cd7df6cc06c1c66190c74ff76fa9f
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-08-08 10:31:48 +0200

    Fix and reactivate Lotka-Volterra 4d Lagrangian Tests.

--- test/lotka_volterra_4d_lagrangian_tests.jl
+++ test/lotka_volterra_4d_lagrangian_tests.jl
@@ -2,7 +2,7 @@ using Test
 using GeometricEquations
 using GeometricIntegrators
 using GeometricIntegrators.SPARK
-using GeometricIntegrators.Utils
+using GeometricSolutions
 
 import GeometricProblems.LotkaVolterra4d
 import GeometricProblems.LotkaVolterra4dLagrangian
@@ -17,6 +17,8 @@ import GeometricProblems.LotkaVolterra4dLagrangian: reference_solution
     ref_ode  = LotkaVolterra4d.lotka_volterra_4d_ode()
     ref_iode = LotkaVolterra4d.lotka_volterra_4d_iode()
     ref_idae = LotkaVolterra4d.lotka_volterra_4d_idae()
+
+    ref = integrate(ref_ode, Gauss(8))
 
     @assert tbegin(lag_ode)  == tbegin(ref_ode)
     @assert tbegin(lag_iode) == tbegin(ref_iode)
@@ -34,21 +36,19 @@ import GeometricProblems.LotkaVolterra4dLagrangian: reference_solution
 
     v1 = zero(lag_ode.ics.q)
     v2 = zero(ref_ode.ics.q)
-    lag_equs[:v](tbegin(lag_ode), lag_ode.ics.q, v1)
-    ref_equs[:v](tbegin(ref_ode), ref_ode.ics.q, v2)
-    @test v1 ≈ v2  atol=1E-14
+    lag_equs[:v](v1, tbegin(lag_ode), lag_ode.ics.q)
+    ref_equs[:v](v2, tbegin(ref_ode), ref_ode.ics.q)
+    @test v1 ≈ v2  atol=eps()
 
     h1 = lag_invs[:h](tbegin(lag_ode), lag_ode.ics.q)
     h2 = ref_invs[:h](tbegin(ref_ode), ref_ode.ics.q)
-    @test h1 ≈ h2  atol=1E-14
+    @test h1 ≈ h2  atol=eps()
 
-    int = Integrator(lag_ode, TableauGauss(2))
-    sol = integrate(lag_ode, int)
-    @test relative_maximum_error(sol.q, reference_solution) < 8E-4
+    sol = integrate(lag_ode, Gauss(2))
+    @test relative_maximum_error(sol.q, ref.q) < 8E-4
 
-    ref_int = Integrator(ref_ode, TableauGauss(2))
-    ref_sol = integrate(ref_ode, ref_int)
-    @test relative_maximum_error(sol.q, ref_sol.q[end]) < 2E-14
+    ref_sol = integrate(ref_ode, Gauss(2))
+    @test relative_maximum_error(sol.q, ref_sol.q) < 2E-14
 
 
     lag_equs = functions(lag_iode)
@@ -60,53 +60,51 @@ import GeometricProblems.LotkaVolterra4dLagrangian: reference_solution
 
     ϑ1 = zero(lag_iode.ics.q)
     ϑ2 = zero(ref_iode.ics.q)
-    lag_equs[:ϑ](tbegin(lag_iode), lag_iode.ics.q, v1, ϑ1)
-    ref_equs[:ϑ](tbegin(ref_iode), ref_iode.ics.q, v2, ϑ2)
-    @test ϑ1 ≈ ϑ2  atol=1E-14
+    lag_equs[:ϑ](ϑ1, tbegin(lag_iode), lag_iode.ics.q, v1)
+    ref_equs[:ϑ](ϑ2, tbegin(ref_iode), ref_iode.ics.q, v2)
+    @test ϑ1 ≈ ϑ2  atol=eps()
 
     f1 = zero(lag_iode.ics.q)
     f2 = zero(ref_iode.ics.q)
-    lag_equs[:f](tbegin(lag_iode), lag_iode.ics.q, v1, f1)
-    ref_equs[:f](tbegin(ref_iode), ref_iode.ics.q, v2, f2)
-    @test f1 ≈ f2  atol=1E-14
+    lag_equs[:f](f1, tbegin(lag_iode), lag_iode.ics.q, v1)
+    ref_equs[:f](f2, tbegin(ref_iode), ref_iode.ics.q, v2)
+    @test f1 ≈ f2  atol=eps()
 
     g1 = zero(lag_iode.ics.q)
     g2 = zero(ref_iode.ics.q)
-    lag_equs[:g](tbegin(lag_iode), lag_iode.ics.q, v1, g1)
-    ref_equs[:g](tbegin(ref_iode), ref_iode.ics.q, v2, g2)
-    @test g1 ≈ g2  atol=1E-14
+    λ1 = zero(v1)
+    λ2 = zero(v2)
+    lag_equs[:g](g1, tbegin(lag_iode), lag_iode.ics.q, v1, λ1)
+    ref_equs[:g](g2, tbegin(ref_iode), ref_iode.ics.q, v2, λ2)
+    @test g1 ≈ g2  atol=eps()
 
     v1 = zero(lag_iode.ics.q)
     v2 = zero(ref_iode.ics.q)
-    lag_equs[:v̄](tbegin(lag_iode), lag_iode.ics.q, v1)
-    ref_equs[:v̄](tbegin(ref_iode), ref_iode.ics.q, v2)
-    @test v1 ≈ v2  atol=1E-14
+    lag_equs[:v̄](v1, tbegin(lag_iode), lag_iode.ics.q, zero(f1))
+    ref_equs[:v̄](v2, tbegin(ref_iode), ref_iode.ics.q, zero(f2))
+    @test v1 ≈ v2  atol=eps()
 
     f1 = zero(lag_iode.ics.q)
     f2 = zero(ref_iode.ics.q)
-    lag_equs[:f̄](tbegin(lag_iode), lag_iode.ics.q, v1, f1)
-    ref_equs[:f̄](tbegin(ref_iode), ref_iode.ics.q, v2, f2)
-    @test f1 ≈ f2  atol=1E-14
+    lag_equs[:f̄](f1, tbegin(lag_iode), lag_iode.ics.q, v1)
+    ref_equs[:f̄](f2, tbegin(ref_iode), ref_iode.ics.q, v2)
+    @test f1 ≈ f2  atol=eps()
 
     h1 = lag_invs[:h](tbegin(lag_iode), lag_iode.ics.q, zero(lag_iode.ics.q))
     h2 = ref_invs[:h](tbegin(ref_iode), ref_iode.ics.q, zero(ref_iode.ics.q))
-    @test h1 ≈ h2  atol=1E-14
+    @test h1 ≈ h2  atol=eps()
 
-    int = IntegratorVPRKpMidpoint(lag_iode, TableauVPGLRK(2))
-    sol = integrate(lag_iode, int)
-    @test relative_maximum_error(sol.q, reference_solution) < 2E-3
+    sol = integrate(lag_iode, MidpointProjection(VPRKGauss(2)))
+    @test relative_maximum_error(sol.q, ref.q) < 2E-3
 
-    ref_int = IntegratorVPRKpMidpoint(ref_iode, TableauVPGLRK(2))
-    ref_sol = integrate(ref_iode, ref_int)
-    @test relative_maximum_error(sol.q, ref_sol.q[end]) < 8E-14
+    ref_sol = integrate(ref_iode, MidpointProjection(VPRKGauss(2)))
+    @test relative_maximum_error(sol.q, ref_sol.q) < 8E-14
 
-    int = IntegratorVPRKpSymmetric(lag_iode, TableauVPGLRK(2))
-    sol = integrate(lag_iode, int)
-    @test relative_maximum_error(sol.q, reference_solution) < 8E-4
+    sol = integrate(lag_iode, SymmetricProjection(VPRKGauss(2)))
+    @test relative_maximum_error(sol.q, ref.q) < 8E-4
 
-    ref_int = IntegratorVPRKpSymmetric(ref_iode, TableauVPGLRK(2))
-    ref_sol = integrate(ref_iode, ref_int)
-    @test relative_maximum_error(sol.q, ref_sol.q[end]) < 4E-14
+    ref_sol = integrate(ref_iode, SymmetricProjection(VPRKGauss(2)))
+    @test relative_maximum_error(sol.q, ref_sol.q) < 4E-14
 
 
     lag_equs = functions(lag_idae)
@@ -118,34 +116,30 @@ import GeometricProblems.LotkaVolterra4dLagrangian: reference_solution
 
     v1 = zero(lag_idae.ics.q)
     v2 = zero(ref_idae.ics.q)
-    lag_equs[:v̄](tbegin(lag_idae), lag_idae.ics.q, v1)
-    ref_equs[:v̄](tbegin(ref_idae), ref_idae.ics.q, v2)
-    @test v1 ≈ v2  atol=1E-14
+    lag_equs[:v̄](v1, tbegin(lag_idae), lag_idae.ics.q, zero(f1))
+    ref_equs[:v̄](v2, tbegin(ref_idae), ref_idae.ics.q, zero(f2))
+    @test v1 ≈ v2  atol=eps()
 
     f1 = zero(lag_idae.ics.q)
     f2 = zero(ref_idae.ics.q)
-    lag_equs[:f̄](tbegin(lag_idae), lag_idae.ics.q, v1, f1)
-    ref_equs[:f̄](tbegin(ref_idae), ref_idae.ics.q, v2, f2)
-    @test f1 ≈ f2  atol=1E-14
+    lag_equs[:f̄](f1, tbegin(lag_idae), lag_idae.ics.q, v1)
+    ref_equs[:f̄](f2, tbegin(ref_idae), ref_idae.ics.q, v2)
+    @test f1 ≈ f2  atol=eps()
 
     h1 = lag_invs[:h](tbegin(lag_idae), lag_idae.ics.q, zero(lag_idae.ics.q))
     h2 = ref_invs[:h](tbegin(ref_idae), ref_idae.ics.q, zero(ref_idae.ics.q))
-    @test h1 ≈ h2  atol=1E-14
+    @test h1 ≈ h2  atol=eps()
 
-    int = Integrator(lag_idae, TableauVSPARKGLRKpMidpoint(2))
-    sol = integrate(lag_idae, int)
-    @test relative_maximum_error(sol.q, reference_solution) < 2E-3
+    sol = integrate(lag_idae, TableauVSPARKGLRKpMidpoint(2))
+    @test relative_maximum_error(sol.q, ref.q) < 2E-3
 
-    ref_int = Integrator(ref_idae, TableauVSPARKGLRKpMidpoint(2))
-    ref_sol = integrate(ref_idae, ref_int)
-    @test relative_maximum_error(sol.q, ref_sol.q[end]) < 4E-14
+    ref_sol = integrate(ref_idae, TableauVSPARKGLRKpMidpoint(2))
+    @test relative_maximum_error(sol.q, ref_sol.q) < 4E-14
 
-    int = Integrator(lag_idae, TableauVSPARKGLRKpSymmetric(2))
-    sol = integrate(lag_idae, int)
-    @test relative_maximum_error(sol.q, reference_solution) < 8E-4
+    sol = integrate(lag_idae, TableauVSPARKGLRKpSymmetric(2))
+    @test relative_maximum_error(sol.q, ref.q) < 8E-4
 
-    ref_int = Integrator(ref_idae, TableauVSPARKGLRKpSymmetric(2))
-    ref_sol = integrate(ref_idae, ref_int)
-    @test relative_maximum_error(sol.q, ref_sol.q[end]) < 8E-14
+    ref_sol = integrate(ref_idae, TableauVSPARKGLRKpSymmetric(2))
+    @test relative_maximum_error(sol.q, ref_sol.q) < 8E-14
 
 end
