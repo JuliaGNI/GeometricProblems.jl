2a0798129a9e53f5c807a6e6a1341cccbdfcbb27
benedict-96 <benedikt.brantner@icloud.com>
2024-04-22 15:58:08 +0200

    Added another option for producing initial conditions for a pulse that travels to the right.

--- src/bump_initial_condition.jl
+++ src/bump_initial_condition.jl
@@ -1,3 +1,5 @@
+using ForwardDiff
+
 """
 Third-degree spline that is used as a basis to construct the initial conditions. 
 """
@@ -11,7 +13,7 @@ function h(x::T) where T
     end 
 end
 
-function s(ξ::T, μ::T) where T 
+function s(ξ::Union{T, ForwardDiff.Dual}, μ::T) where T <: Real
     20 * μ * abs(ξ + μ / 2)
 end
 
@@ -24,14 +26,34 @@ function u₀(ξ::AbstractVector{T}, μ::T) where T
     h.(s(ξ, μ))
 end
 
-u₀(ξ, μ) = h(s(ξ, μ))
+u₀(ξ::Union{T, ForwardDiff.Dual}, μ::T) where T <: Real = h(s(ξ, μ))
 
-function get_domain(Ñ::Integer, T=Float64)
-    Δx = 1. / (Ñ - 1)
+function get_domain(N::Integer, T=Float64)
+    Δx = 1. / (N - 1)
     T(-0.5) : Δx : T(0.5)
 end
 
-function get_initial_condition(μ::T, Ñ::Integer) where T 
-    Ω = get_domain(Ñ, T)
+function get_p₀(ξ::T, μ::T) where T <: Real
+    - μ * ForwardDiff.derivative(ξ -> u₀(ξ, μ), ξ)
+end
+
+function get_p₀(Ω::AbstractVector{T}, μ::T) where T 
+    p₀_closure(ξ::T) = get_p₀(ξ, μ)
+    p₀_closure.(Ω)
+end
+
+@doc raw"""
+Produces initial conditions for the bump function. Here the ``p``-part is initialized with zeros. 
+"""
+function get_initial_condition(μ::T, N::Integer) where T 
+    Ω = get_domain(N, T)
     (q =u₀(Ω, μ), p = zero(Ω))
+end 
+
+@doc raw"""
+Produces initial condition for the bump function. Here the ``p``-part is initialized as ``-\mu\partial_\xi u_0(\xi, \mu)``. 
+"""
+function get_initial_condition2(μ::T, N::Integer) where T 
+    Ω = get_domain(N, T)
+    (q =u₀(Ω, μ), p = get_p₀(Ω, μ))
 end 
