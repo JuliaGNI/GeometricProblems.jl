476bdbfe8f8ed0eb2bb1e87736222bb62037da2c
benedict-96 <benedikt.brantner@icloud.com>
2024-04-23 10:20:56 +0200

    h -> \partial{}h.

--- test/bump_initial_condition_test_derivatives.jl
+++ test/bump_initial_condition_test_derivatives.jl
@@ -1,26 +1,28 @@
-using GeometricProblems.LinearWave: h, s, u₀, ∂h, ∂s, ∂u₀
+import GeometricProblems.LinearWave as lw
+using GeometricProblems: h, s, u₀, ∂h, ∂s, ∂u₀
 using ForwardDiff
 using Test
 
 const test_point₁ = 0.5
 const test_point₂ = 1.5
 const test_point₃ = 2.5
+const μ = lw.default_parameters.μ
 
 function test_h_derivative(test_point)
     h_autodiff = ForwardDiff.derivative(h, test_point)
-    @test h_autodiff ≈ h(test_point)
+    @test h_autodiff ≈ ∂h(test_point)
 end
 
 function test_s_derivative(test_point)
-    s_closure(ξ) = s(ξ, lw.μ)
+    s_closure(ξ) = s(ξ, μ)
     s_autodiff = ForwardDiff.derivative(s_closure, test_point)
-    @test s_autodiff ≈ ∂s(test_point, lw.μ)
+    @test s_autodiff ≈ ∂s(test_point, μ)
 end
 
 function test_u₀_derivative(test_point)
-    u₀_closure(ξ) = u₀(ξ, lw.μ)
+    u₀_closure(ξ) = u₀(ξ, μ)
     u₀_autodiff = ForwardDiff.derivative(u₀_closure, test_point)
-    @test u₀_autodiff ≈ ∂u₀(test_point, lw.μ)
+    @test u₀_autodiff ≈ ∂u₀(test_point, μ)
 end
 
 function test_all_derivatives(test_point)
