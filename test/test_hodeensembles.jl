eca602a6a0285e246febfb2110e7a4df5c021725
benedict-96 <benedikt.brantner@icloud.com>
2024-07-08 13:43:40 +0200

    Only computing two instances now in the parameter case.

--- test/test_hodeensembles.jl
+++ test/test_hodeensembles.jl
@@ -12,9 +12,7 @@ function test_multiple_initial_conditions(cho::Module)
     # ensemble solution
     esol = integrate(epr, ImplicitMidpoint())
 
-    sol = integrate(cho.hodeproblem(), ImplicitMidpoint())
-    @test esol.s[1].q.d.parent ≈ sol.q.d.parent
-    @test esol.s[2].q.d.parent ≉ sol.q.d.parent
+    @test esol.s[2].q.d.parent ≉ esol.s[1].q.d.parent
 end
 
 function test_multiple_parameters(cho::Module)
@@ -22,9 +20,10 @@ function test_multiple_parameters(cho::Module)
 
     for i in 0:1
         param_vals = ()
-        for key in keys(cho.default_parameters)
-            param_vals = (param_vals..., cho.default_parameters[key] .+ 1. * i)
-        end
+
+        # take the first parameter (compute the ensemble by changing this parameter)
+        key = keys(cho.default_parameters)[1]
+    
         params_vec = push!(params_vec, NamedTuple{keys(cho.default_parameters)}(param_vals))
     end
 
@@ -33,9 +32,7 @@ function test_multiple_parameters(cho::Module)
     # ensemble solution
     esol = integrate(epr, ImplicitMidpoint())
 
-    sol = integrate(cho.hodeproblem(), ImplicitMidpoint())
-    @test esol.s[1].q.d.parent ≈ sol.q.d.parent
-    @test esol.s[2].q.d.parent ≉ sol.q.d.parent
+    @test esol.s[2].q.d.parent ≉ esol.s[1].q.d.parent
 end
 
 test_multiple_initial_conditions(cho)
