9b4ea6d3a03a98daf39e47015adae9cdaef15d09
Michael Kraus <michael.kraus@ipp.mpg.de>
2024-12-12 11:37:50 +0100

    Generate Lotka-Volterra 4D Euler-Lagrange equations using EulerLagrange.jl instead of Symbolics.jl.

--- test/lotka_volterra_4d_lagrangian_tests.jl
+++ test/lotka_volterra_4d_lagrangian_tests.jl
@@ -11,22 +11,22 @@ import GeometricProblems.LotkaVolterra4dLagrangian: reference_solution
 
 @testset "$(rpad("Lotka-Volterra 4D (Lagrangian)",80))" begin
     lag_ode  = LotkaVolterra4dLagrangian.odeproblem()
-    lag_iode = LotkaVolterra4dLagrangian.iodeproblem()
-    lag_idae = LotkaVolterra4dLagrangian.idaeproblem()
+    lag_lode = LotkaVolterra4dLagrangian.lodeproblem()
+    lag_ldae = LotkaVolterra4dLagrangian.ldaeproblem()
 
     ref_ode  = LotkaVolterra4d.odeproblem()
-    ref_iode = LotkaVolterra4d.iodeproblem()
-    ref_idae = LotkaVolterra4d.idaeproblem()
+    ref_lode = LotkaVolterra4d.lodeproblem()
+    ref_ldae = LotkaVolterra4d.ldaeproblem()
 
     ref = integrate(ref_ode, Gauss(8))
 
-    @assert tbegin(lag_ode)  == tbegin(ref_ode)
-    @assert tbegin(lag_iode) == tbegin(ref_iode)
-    @assert tbegin(lag_idae) == tbegin(ref_idae)
+    @test tbegin(lag_ode)  == tbegin(ref_ode)
+    @test tbegin(lag_lode) == tbegin(ref_lode)
+    @test tbegin(lag_ldae) == tbegin(ref_ldae)
 
-    @assert lag_ode.ics  == ref_ode.ics
-    @assert lag_iode.ics == ref_iode.ics
-    @assert lag_idae.ics == ref_idae.ics
+    @test lag_ode.ics  == ref_ode.ics
+    @test lag_lode.ics == ref_lode.ics
+    @test lag_ldae.ics == ref_ldae.ics
 
 
     lag_equs = functions(lag_ode)
@@ -36,116 +36,151 @@ import GeometricProblems.LotkaVolterra4dLagrangian: reference_solution
 
     v1 = zero(lag_ode.ics.q)
     v2 = zero(ref_ode.ics.q)
-    lag_equs[:v](v1, tbegin(lag_ode), lag_ode.ics.q, parameters(lag_ode))
-    ref_equs[:v](v2, tbegin(ref_ode), ref_ode.ics.q, parameters(ref_ode))
+    lag_equs.v(v1, tbegin(lag_ode), lag_ode.ics.q, parameters(lag_ode))
+    ref_equs.v(v2, tbegin(ref_ode), ref_ode.ics.q, parameters(ref_ode))
     @test v1 ≈ v2  atol=eps()
 
-    h1 = lag_invs[:h](tbegin(lag_ode), lag_ode.ics.q, parameters(lag_ode))
-    h2 = ref_invs[:h](tbegin(ref_ode), ref_ode.ics.q, parameters(ref_ode))
+    h1 = lag_invs.h(tbegin(lag_ode), lag_ode.ics.q, parameters(lag_ode))
+    h2 = ref_invs.h(tbegin(ref_ode), ref_ode.ics.q, parameters(ref_ode))
     @test h1 ≈ h2  atol=eps()
 
-    sol = integrate(lag_ode, Gauss(2))
-    @test relative_maximum_error(sol.q, ref.q) < 8E-4
+    lag_sol = integrate(lag_ode, Gauss(2))
+    @test relative_maximum_error(lag_sol.q, ref.q) < 1E-8
 
     ref_sol = integrate(ref_ode, Gauss(2))
-    @test relative_maximum_error(sol.q, ref_sol.q) < 2E-14
+    @test relative_maximum_error(lag_sol.q, ref_sol.q) < 1E-14
 
 
-    lag_equs = functions(lag_iode)
-    lag_invs = invariants(lag_iode)
-    lag_igs  = initialguess(lag_iode)
-    ref_equs = functions(ref_iode)
-    ref_invs = invariants(ref_iode)
+    lag_equs = functions(lag_lode)
+    lag_invs = invariants(lag_lode)
+    lag_igs  = initialguess(lag_lode)
+    ref_equs = functions(ref_lode)
+    ref_invs = invariants(ref_lode)
 
-    @test lag_iode.ics.q == ref_iode.ics.q
-    @test lag_iode.ics.p == ref_iode.ics.p
-    @test parameters(lag_iode) == parameters(ref_iode)
+    @test lag_lode.ics.q == ref_lode.ics.q
+    @test lag_lode.ics.p == ref_lode.ics.p
+    @test parameters(lag_lode) == parameters(ref_lode)
 
-    ϑ1 = zero(lag_iode.ics.q)
-    ϑ2 = zero(ref_iode.ics.q)
-    lag_equs[:ϑ](ϑ1, tbegin(lag_iode), lag_iode.ics.q, v1, parameters(lag_iode))
-    ref_equs[:ϑ](ϑ2, tbegin(ref_iode), ref_iode.ics.q, v2, parameters(ref_iode))
+    ϑ1 = zero(lag_lode.ics.q)
+    ϑ2 = zero(ref_lode.ics.q)
+    lag_equs.ϑ(ϑ1, tbegin(lag_lode), lag_lode.ics.q, v1, parameters(lag_lode))
+    ref_equs.ϑ(ϑ2, tbegin(ref_lode), ref_lode.ics.q, v2, parameters(ref_lode))
     @test ϑ1 ≈ ϑ2  atol=eps()
 
-    f1 = zero(lag_iode.ics.q)
-    f2 = zero(ref_iode.ics.q)
-    lag_equs[:f](f1, tbegin(lag_iode), lag_iode.ics.q, v1, parameters(lag_iode))
-    ref_equs[:f](f2, tbegin(ref_iode), ref_iode.ics.q, v2, parameters(ref_iode))
+    f1 = zero(lag_lode.ics.q)
+    f2 = zero(ref_lode.ics.q)
+    lag_equs.f(f1, tbegin(lag_lode), lag_lode.ics.q, v1, parameters(lag_lode))
+    ref_equs.f(f2, tbegin(ref_lode), ref_lode.ics.q, v2, parameters(ref_lode))
     @test f1 ≈ f2  atol=eps()
 
-    g1 = zero(lag_iode.ics.q)
-    g2 = zero(ref_iode.ics.q)
-    λ1 = zero(v1)
-    λ2 = zero(v2)
-    lag_equs[:g](g1, tbegin(lag_iode), lag_iode.ics.q, v1, λ1, parameters(lag_iode))
-    ref_equs[:g](g2, tbegin(ref_iode), ref_iode.ics.q, v2, λ2, parameters(ref_iode))
+    g1 = zero(lag_lode.ics.q)
+    g2 = zero(ref_lode.ics.q)
+    lag_equs.g(g1, tbegin(lag_lode), lag_lode.ics.q, v1, zero(v1), parameters(lag_lode))
+    ref_equs.g(g2, tbegin(ref_lode), ref_lode.ics.q, v2, zero(v2), parameters(ref_lode))
     @test g1 ≈ g2  atol=eps()
 
-    v1 = zero(lag_iode.ics.q)
-    v2 = zero(ref_iode.ics.q)
-    lag_igs[:v](v1, tbegin(lag_iode), lag_iode.ics.q, zero(f1), parameters(lag_iode))
-    lag_igs[:v](v2, tbegin(ref_iode), ref_iode.ics.q, zero(f2), parameters(ref_iode))
+    v1 = zero(lag_lode.ics.q)
+    v2 = zero(ref_lode.ics.q)
+    lag_igs.v(v1, tbegin(lag_lode), lag_lode.ics.q, zero(f1), parameters(lag_lode))
+    lag_igs.v(v2, tbegin(ref_lode), ref_lode.ics.q, zero(f2), parameters(ref_lode))
     @test v1 ≈ v2  atol=eps()
 
-    f1 = zero(lag_iode.ics.q)
-    f2 = zero(ref_iode.ics.q)
-    lag_igs[:f](f1, tbegin(lag_iode), lag_iode.ics.q, v1, parameters(lag_iode))
-    lag_igs[:f](f2, tbegin(ref_iode), ref_iode.ics.q, v2, parameters(ref_iode))
+    f1 = zero(lag_lode.ics.q)
+    f2 = zero(ref_lode.ics.q)
+    lag_igs.f(f1, tbegin(lag_lode), lag_lode.ics.q, v1, parameters(lag_lode))
+    lag_igs.f(f2, tbegin(ref_lode), ref_lode.ics.q, v2, parameters(ref_lode))
     @test f1 ≈ f2  atol=eps()
 
-    h1 = lag_invs[:h](tbegin(lag_iode), lag_iode.ics.q, zero(lag_iode.ics.q), parameters(lag_iode))
-    h2 = ref_invs[:h](tbegin(ref_iode), ref_iode.ics.q, zero(ref_iode.ics.q), parameters(ref_iode))
+    h1 = lag_invs.h(tbegin(lag_lode), lag_lode.ics.q, lag_lode.ics.v, parameters(lag_lode))
+    h2 = ref_invs.h(tbegin(ref_lode), ref_lode.ics.q, ref_lode.ics.v, parameters(ref_lode))
     @test h1 ≈ h2  atol=eps()
 
-    sol = integrate(lag_iode, MidpointProjection(VPRKGauss(2)))
-    @test relative_maximum_error(sol.q, ref.q) < 2E-3
+    lag_sol = integrate(lag_lode, Gauss(2))
+    @test relative_maximum_error(lag_sol.q, ref.q) < 8E-4
 
-    ref_sol = integrate(ref_iode, MidpointProjection(VPRKGauss(2)))
-    @test relative_maximum_error(sol.q, ref_sol.q) < 8E-14
+    ref_sol = integrate(ref_lode, Gauss(2))
+    @test relative_maximum_error(ref_sol.q, ref.q) < 8E-4
+    @test relative_maximum_error(ref_sol.q, lag_sol.q) < 4E-13
 
-    sol = integrate(lag_iode, SymmetricProjection(VPRKGauss(2)))
-    @test relative_maximum_error(sol.q, ref.q) < 8E-4
+    lag_sol = integrate(lag_lode, MidpointProjection(VPRKGauss(2)))
+    ref_sol = integrate(ref_lode, MidpointProjection(VPRKGauss(2)))
+    @test relative_maximum_error(lag_sol.q, ref.q) < 1E-8
+    @test relative_maximum_error(ref_sol.q, ref.q) < 1E-8
+    @test relative_maximum_error(ref_sol.q, lag_sol.q) < 4E-14
 
-    ref_sol = integrate(ref_iode, SymmetricProjection(VPRKGauss(2)))
-    @test relative_maximum_error(sol.q, ref_sol.q) < 4E-14
+    lag_sol = integrate(lag_lode, SymmetricProjection(VPRKGauss(2)))
+    ref_sol = integrate(ref_lode, SymmetricProjection(VPRKGauss(2)))
+    @test relative_maximum_error(lag_sol.q, ref.q) < 1E-8
+    @test relative_maximum_error(ref_sol.q, ref.q) < 1E-8
+    @test relative_maximum_error(ref_sol.q, lag_sol.q) < 1E-14
 
 
-    lag_equs = functions(lag_idae)
-    lag_invs = invariants(lag_idae)
-    lag_igs  = initialguess(lag_idae)
-    ref_equs = functions(ref_idae)
-    ref_invs = invariants(ref_idae)
+    lag_equs = functions(lag_ldae)
+    lag_invs = invariants(lag_ldae)
+    lag_igs  = initialguess(lag_ldae)
+    ref_equs = functions(ref_ldae)
+    ref_invs = invariants(ref_ldae)
 
-    @test lag_idae.ics.q == ref_idae.ics.q
-    @test lag_idae.ics.p == ref_idae.ics.p
-    @test parameters(lag_idae) == parameters(ref_idae)
+    @test lag_ldae.ics.q == ref_ldae.ics.q
+    @test lag_ldae.ics.p == ref_ldae.ics.p
+    @test parameters(lag_ldae) == parameters(ref_ldae)
 
-    v1 = zero(lag_idae.ics.q)
-    v2 = zero(ref_idae.ics.q)
-    lag_igs[:v](v1, tbegin(lag_idae), lag_idae.ics.q, zero(f1), parameters(lag_idae))
-    lag_igs[:v](v2, tbegin(ref_idae), ref_idae.ics.q, zero(f2), parameters(ref_idae))
+    ϑ1 = zero(lag_ldae.ics.q)
+    ϑ2 = zero(ref_ldae.ics.q)
+    lag_equs.ϑ(ϑ1, tbegin(lag_ldae), lag_ldae.ics.q, v1, parameters(lag_ldae))
+    ref_equs.ϑ(ϑ2, tbegin(ref_ldae), ref_ldae.ics.q, v2, parameters(ref_ldae))
+    @test ϑ1 ≈ ϑ2  atol=eps()
+
+    f1 = zero(lag_ldae.ics.q)
+    f2 = zero(ref_ldae.ics.q)
+    lag_equs.f(f1, tbegin(lag_ldae), lag_ldae.ics.q, v1, parameters(lag_ldae))
+    ref_equs.f(f2, tbegin(ref_ldae), ref_ldae.ics.q, v2, parameters(ref_ldae))
+    @test f1 ≈ f2  atol=eps()
+
+    g1 = zero(lag_ldae.ics.q)
+    g2 = zero(ref_ldae.ics.q)
+    lag_equs.g(g1, tbegin(lag_ldae), lag_ldae.ics.q, v1, lag_ldae.ics.p, lag_ldae.ics.λ, parameters(lag_ldae))
+    ref_equs.g(g2, tbegin(ref_ldae), ref_ldae.ics.q, v2, ref_ldae.ics.p, ref_ldae.ics.λ, parameters(ref_ldae))
+    @test g1 ≈ g2  atol=eps()
+
+    u1 = zero(lag_ldae.ics.q)
+    u2 = zero(ref_ldae.ics.q)
+    lag_equs.u(u1, tbegin(lag_ldae), lag_ldae.ics.q, v1, lag_ldae.ics.p, lag_ldae.ics.λ, parameters(lag_ldae))
+    ref_equs.u(u2, tbegin(ref_ldae), ref_ldae.ics.q, v2, ref_ldae.ics.p, ref_ldae.ics.λ, parameters(ref_ldae))
+    @test u1 ≈ u2  atol=eps()
+
+    ϕ1 = zero(lag_ldae.ics.q)
+    ϕ2 = zero(ref_ldae.ics.q)
+    lag_equs.ϕ(ϕ1, tbegin(lag_ldae), lag_ldae.ics.q, v1, lag_ldae.ics.p, parameters(lag_ldae))
+    ref_equs.ϕ(ϕ2, tbegin(ref_ldae), ref_ldae.ics.q, v2, ref_ldae.ics.p, parameters(ref_ldae))
+    @test ϕ1 ≈ ϕ2  atol=eps()
+
+    v1 = zero(lag_ldae.ics.q)
+    v2 = zero(ref_ldae.ics.q)
+    lag_igs.v(v1, tbegin(lag_ldae), lag_ldae.ics.q, zero(f1), parameters(lag_ldae))
+    lag_igs.v(v2, tbegin(ref_ldae), ref_ldae.ics.q, zero(f2), parameters(ref_ldae))
     @test v1 ≈ v2  atol=eps()
 
-    f1 = zero(lag_idae.ics.q)
-    f2 = zero(ref_idae.ics.q)
-    lag_igs[:f](f1, tbegin(lag_idae), lag_idae.ics.q, v1, parameters(lag_idae))
-    lag_igs[:f](f2, tbegin(ref_idae), ref_idae.ics.q, v2, parameters(ref_idae))
+    f1 = zero(lag_ldae.ics.q)
+    f2 = zero(ref_ldae.ics.q)
+    lag_igs.f(f1, tbegin(lag_ldae), lag_ldae.ics.q, v1, parameters(lag_ldae))
+    lag_igs.f(f2, tbegin(ref_ldae), ref_ldae.ics.q, v2, parameters(ref_ldae))
     @test f1 ≈ f2  atol=eps()
 
-    h1 = lag_invs[:h](tbegin(lag_idae), lag_idae.ics.q, zero(lag_idae.ics.q), parameters(lag_idae))
-    h2 = ref_invs[:h](tbegin(ref_idae), ref_idae.ics.q, zero(ref_idae.ics.q), parameters(ref_idae))
+    h1 = lag_invs.h(tbegin(lag_ldae), lag_ldae.ics.q, zero(lag_ldae.ics.q), parameters(lag_ldae))
+    h2 = ref_invs.h(tbegin(ref_ldae), ref_ldae.ics.q, zero(ref_ldae.ics.q), parameters(ref_ldae))
     @test h1 ≈ h2  atol=eps()
 
-    sol = integrate(lag_idae, TableauVSPARKGLRKpMidpoint(2))
-    @test relative_maximum_error(sol.q, ref.q) < 2E-3
+    lag_sol = integrate(lag_ldae, TableauVSPARKGLRKpMidpoint(2))
+    ref_sol = integrate(ref_ldae, TableauVSPARKGLRKpMidpoint(2))
+    @test relative_maximum_error(lag_sol.q, ref.q) < 1E-8
+    @test relative_maximum_error(ref_sol.q, ref.q) < 1E-8
+    @test relative_maximum_error(lag_sol.q, ref_sol.q) < 1E-14
 
-    ref_sol = integrate(ref_idae, TableauVSPARKGLRKpMidpoint(2))
-    @test relative_maximum_error(sol.q, ref_sol.q) < 4E-14
-
-    sol = integrate(lag_idae, TableauVSPARKGLRKpSymmetric(2))
-    @test relative_maximum_error(sol.q, ref.q) < 8E-4
-
-    ref_sol = integrate(ref_idae, TableauVSPARKGLRKpSymmetric(2))
-    @test relative_maximum_error(sol.q, ref_sol.q) < 8E-14
+    lag_sol = integrate(lag_ldae, TableauVSPARKGLRKpSymmetric(2))
+    ref_sol = integrate(ref_ldae, TableauVSPARKGLRKpSymmetric(2))
+    @test relative_maximum_error(lag_sol.q, ref.q) < 1E-8
+    @test relative_maximum_error(ref_sol.q, ref.q) < 1E-8
+    @test relative_maximum_error(lag_sol.q, ref_sol.q) < 8E-14
 
 end
