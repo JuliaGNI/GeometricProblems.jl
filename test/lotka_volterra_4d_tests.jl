d9eda7ab93208fb566774f4aa9dae70a87dab226
Michael Kraus <michael.kraus@ipp.mpg.de>
2024-12-12 11:37:21 +0100

    Fixes in Lotka-Volterra 4D Lagrangian and add more tests.

--- test/lotka_volterra_4d_tests.jl
+++ test/lotka_volterra_4d_tests.jl
@@ -6,23 +6,71 @@ using GeometricSolutions
 
 
 @testset "$(rpad("Lotka-Volterra 4D",80))" begin
+
     ode  = odeproblem()
     iode = iodeproblem()
+    lode = lodeproblem()
+    pode = podeproblem()
     idae = idaeproblem()
+    ldae = ldaeproblem()
+    pdae = pdaeproblem()
+
+    ldae_secondary = ldaeproblem_secondary()
+
     ref  = integrate(ode, Gauss(8))
 
+
     sol = integrate(ode, Gauss(2))
+    @test relative_maximum_error(sol.q, ref.q) < 1E-8
+
+    sol = integrate(iode, Gauss(2))
     @test relative_maximum_error(sol.q, ref.q) < 8E-4
+
+    sol = integrate(lode, Gauss(2))
+    @test relative_maximum_error(sol.q, ref.q) < 8E-4
+
+    sol = integrate(pode, Gauss(2))
+    @test relative_maximum_error(sol.q, ref.q) < 1E-8
+
+    sol = integrate(idae, Gauss(2))
+    @test relative_maximum_error(sol.q, ref.q) < 8E-4
+
+    sol = integrate(ldae, Gauss(2))
+    @test relative_maximum_error(sol.q, ref.q) < 8E-4
+
+    sol = integrate(pdae, Gauss(2))
+    @test relative_maximum_error(sol.q, ref.q) < 1E-8
+
 
     sol = integrate(iode, MidpointProjection(VPRKGauss(2)))
-    @test relative_maximum_error(sol.q, ref.q) < 2E-3
+    @test relative_maximum_error(sol.q, ref.q) < 1E-8
 
     sol = integrate(iode, SymmetricProjection(VPRKGauss(2)))
-    @test relative_maximum_error(sol.q, ref.q) < 8E-4
+    @test relative_maximum_error(sol.q, ref.q) < 1E-8
+
+    sol = integrate(lode, MidpointProjection(VPRKGauss(2)))
+    @test relative_maximum_error(sol.q, ref.q) < 1E-8
+
+    sol = integrate(lode, SymmetricProjection(VPRKGauss(2)))
+    @test relative_maximum_error(sol.q, ref.q) < 1E-8
 
     sol = integrate(idae, TableauVSPARKGLRKpMidpoint(2))
-    @test relative_maximum_error(sol.q, ref.q) < 2E-3
+    @test relative_maximum_error(sol.q, ref.q) < 1E-8
 
     sol = integrate(idae, TableauVSPARKGLRKpSymmetric(2))
-    @test relative_maximum_error(sol.q, ref.q) < 8E-4
+    @test relative_maximum_error(sol.q, ref.q) < 1E-8
+
+    sol = integrate(ldae, TableauVSPARKGLRKpMidpoint(2))
+    @test relative_maximum_error(sol.q, ref.q) < 1E-8
+
+    sol = integrate(ldae, TableauVSPARKGLRKpSymmetric(2))
+    @test relative_maximum_error(sol.q, ref.q) < 1E-8
+
+
+    sol = integrate(ldae_secondary, TableauVSPARKLobattoIIIE(2))
+    @test relative_maximum_error(sol.q, ref.q) < 4E-5
+    
+    sol = integrate(pdae, TableauHSPARKLobattoIIIE(2))
+    @test relative_maximum_error(sol.q, ref.q) < 4E-4
+
 end
